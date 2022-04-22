b6c41e150c0277ac8ef8abd50f20d26f972d9309
Christopher Shaw <chris@chris-shaw.com>
2022-04-22 13:59:47 +0100

    Refactor

--- GetResidualsForProduct/index.ts
+++ GetResidualsForProduct/index.ts
@@ -1,5 +1,9 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
+const BASE_VALUE = 10000;
+const MILEAGE_OFFSET_MULTIPLIER = 500;
+const REQUIRED_INPUT_KEYS = ['capCode','regDate','annualMiles','currentMiles', 'term'];
+
 function getVehicleAgeInMonths(regDate) {
     let dateFrom = new Date;
     let dateTo = new Date(regDate);
@@ -9,9 +13,8 @@ function getVehicleAgeInMonths(regDate) {
 }
 
 function validateInput (input) {
-    let requiredInput = ['capCode','regDate','annualMiles','currentMiles', 'term'];
     let errors = [];
-    for (let field of requiredInput) {
+    for (let field of REQUIRED_INPUT_KEYS) {
         if (!input[field]) {
             errors.push('Missing Field ' + field); 
         }
@@ -20,16 +23,22 @@ function validateInput (input) {
 }
 
 function getGuarenteedFutureValue (input) {
-    let baseValue = 10000;
+    let baseValue = BASE_VALUE;
     let term = parseInt(input['term']);
 
     let annualMiles = parseInt(input['annualMiles']);
     let currentMiles = parseInt(input['currentMiles']);
     let endMileage = Math.round(currentMiles + ( term * (annualMiles/12) ));
 
+
+    let mileageOffset = (baseValue / endMileage) * MILEAGE_OFFSET_MULTIPLIER;
+
+
     let vehicleAgeInMonths = getVehicleAgeInMonths(input['regDate']);
     let vehicleAgeInMonthsAtEnd = vehicleAgeInMonths + term;
-    return  Math.round(baseValue - (baseValue / 120 * vehicleAgeInMonthsAtEnd));
+
+    let modifiedValue = (baseValue - (baseValue / 120 * vehicleAgeInMonthsAtEnd));
+    return Math.round(modifiedValue - mileageOffset);
 }
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
